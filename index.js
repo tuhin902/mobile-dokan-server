cb3da640622cce8274ecaa7797de60e1ea90e9ad
tuhin902 <tuhin902md@gmail.com>
2022-12-01 18:21:42 +0600

    usercollection added

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
         const categoriesCollection = client.db('mobiledb').collection('categories');
         const productsCollection = client.db('mobiledb').collection('products');
         const bookingsCollection = client.db('mobiledb').collection('bookings');
+        const usersCollection = client.db('mobiledb').collection('users');
 
 
         app.get('/categories', async (req, res) => {
@@ -34,12 +35,26 @@ async function run() {
         });
 
         //booking
+
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        });
+
         app.post('/bookings', async (req, res) => {
             const bookings = req.body;
             const result = await bookingsCollection.insertOne(bookings);
             res.send(result);
 
         });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
 
     }
     finally {
